fbadce163f36e3d96dbcfc1ee226fb8e917c7f9d
Mike Zhang <Mike.Zhang@126.com>
2023-09-27 11:02:06 +0800

    Microservice&Docker&K8S

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,18 +1,37 @@
 using Microsoft.EntityFrameworkCore;
+using PlatformService.AsyncDataServices;
+using PlatformService.AsyncDataServices.Grpc;
 using PlatformService.Data;
+using PlatformService.SyncDatasServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine($"--> Using SqlServer Db");
+    Console.WriteLine($"--> Connection: {builder.Configuration.GetConnectionString("PlatformsConn")}");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine($"--> Using InMemory Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemory"));
+}
 
+builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddGrpc();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -25,5 +44,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
+app.MapGet("/protos/platforms.proto", async context => 
+{
+    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+});
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 
 app.Run();
